25b909da8b02ed0c7ce4801c4c19046645c82431
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-09 13:27:58 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -12,7 +12,7 @@
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
-        include('c:/xampp/htdocs/Midterm_Project/errors/database_error.php');
+        include('../errors/database_error.php');
         exit();
     }
 ?>

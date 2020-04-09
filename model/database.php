a50783093fe1136b7cd3101e8ae8da428859a266
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-09 15:11:17 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
     
-    $dsn = 'mysql:host=wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname='hgr35rs7vhgydaqa';
+    $dsn = 'mysql:host=wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hgr35rs7vhgydaqa';
     $username = 'g02d3awnk7yz8v89';
     $password = 'gmfyrukark3oxzcd';
     try {

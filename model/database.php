1c3e3f74fe2d686fa09d20b0157924c84f7fd81c
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-09 15:05:52 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,8 +1,4 @@
 <?php
-
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = 'Mongmo12';
     
     $dsn = 'mysql:host=wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname='hgr35rs7vhgydaqa';
     $username = 'g02d3awnk7yz8v89';

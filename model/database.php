123e5ef3b32c26d12f8dc67f1639bd8dcbb8ea91
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-09 13:20:30 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,8 +1,12 @@
 <?php
-    
-    $dsn = 'mysql:host=wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname='hgr35rs7vgydaqa';
+
+    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $username = 'root';
     $password = 'Mongmo12';
+    
+    $dsn = 'mysql:host=wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname='hgr35rs7vhgydaqa';
+    $username = 'g02d3awnk7yz8v89';
+    $password = 'gmfyrukark3oxzcd';
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
